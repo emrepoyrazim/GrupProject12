3314c965d193d08528324081e229b9b04ca294db
emre <emrepoyrazim@gmail.com>
2022-10-20 20:19:30 +0300

    basic pom.xml and login page has been added

---
+++ src/main/java/utils/commonMethods.java
@@ -0,0 +1,34 @@
+package utils;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class commonMethods {
+    @FindBy(id="txtUsername")
+    public WebElement usernameBox;
+
+    @FindBy (name="txtPassword")
+    public WebElement passwordBox;
+
+    @FindBy (id="btnLogin")
+    public WebElement loginBtn;
+
+    @FindBy (xpath="//*[@id=\"divLogo\"]/img")
+    public WebElement logo;
+
+    @FindBy ( id="spanMessage")
+    public WebElement errorMssage;
+
+    public LoginPage(){
+        PageFactory.initElements(driver, this);
+    }
+    public  void LoginMethod(String username , String password){
+        sendText(usernameBox,username);
+        sendText(passwordBox,password);
+        loginBtn.click();
+
+    }
+
+
+}
